4f9dc0930139817cd87242150143de03ed6b2b52
SarvT <91484197+SarvT@users.noreply.github.com>
2025-03-17 10:01:02 +0530

    re-configured for prod

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const apiRoute = require("./api/verify.route")
-const serverless = require("serverless-http");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const apiRoute = require("./api/verify.route");
 
 dotenv.config();
-
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", apiRoute)
+app.use("/api", apiRoute);
 app.get("/", (req, res) => {
   res.send("You're at Tasker!");
 });
@@ -20,5 +18,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
